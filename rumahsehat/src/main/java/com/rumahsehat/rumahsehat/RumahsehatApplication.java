66ffced295de6d7117a884ace29f7402db1a63cc
setasena.randata <setasena.randata@ui.ac.id>
2022-11-15 23:53:31 +0700

    init spring boot

---
+++ rumahsehat/src/main/java/com/rumahsehat/rumahsehat/RumahsehatApplication.java
@@ -0,0 +1,13 @@
+package com.rumahsehat.rumahsehat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahsehatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahsehatApplication.class, args);
+	}
+
+}
---
+++ rumahsehat/src/test/java/com/rumahsehat/rumahsehat/RumahsehatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumahsehat.rumahsehat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahsehatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
