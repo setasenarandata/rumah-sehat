5a3f7d9e1b125d19e0a04714f611df4c1dd67050
nathasya.shalsabilla <nathasya.shalsabilla@ui.ac.id>
2022-12-12 10:33:24 +0700

    pull main

--- rumahsehat_mobile/lib/screens/tagihan/tagihan_model.dart
+++ rumahsehat_mobile/lib/screens/tagihan/tagihan_model.dart
@@ -1,20 +1,30 @@
 class TagihanModel {
   final String kode;
-  final bool status;
-  final DateTime tanggalTerbuat;
+  final bool isPaid;
+  final String tanggalTerbuat;
   final int jumlahTagihan;
 
   const TagihanModel({
     required this.kode,
-    required this.jumlahTagihan,
+    required this.isPaid,
     required this.tanggalTerbuat,
-    required this.status;
+    required this.jumlahTagihan,
   });
 
   factory TagihanModel.fromJson(Map<String?, dynamic> json) {
     return TagihanModel(
       kode: json['kode'],
+      isPaid: json['isPaid'],
+      tanggalTerbuat: json['tanggalTerbuat'],
       jumlahTagihan: json['jumlahTagihan']
     );
+  }
+
+  String getStatus() {
+    if (this.isPaid) {
+      return "Lunas";
+    } else {
+      return "Belum Lunas";
+    }
   }
 }
